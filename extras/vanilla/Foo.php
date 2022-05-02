584cb578d84aed1928652e58bde1e10c943b862f
Marcos <exegeses@gmail.com>
2022-05-02 20:05:13 -0300

    Notas Polícias de nombres de Modelos - Eloquent

--- extras/vanilla/Foo.php
+++ extras/vanilla/Foo.php
@@ -1,1 +1,18 @@
 <?php
+
+    class Foo
+    {
+
+
+        /**
+         * @param int $n1
+         * @param int $n2
+         * @return string
+         */
+        public function suma(int $n1, int $n2 ) : string
+        {
+            return '';
+        }
+
+
+    }
